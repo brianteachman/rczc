8cb4d242fb2299b1ea00079c91675d288ee7b3b0
Brian Teachman <me@briant.me>
2013-04-30 22:23:26 -0700

    Reconfigured template renderer.

--- module/Mailer/src/Mailer/Controller/MailController.php
+++ module/Mailer/src/Mailer/Controller/MailController.php
@@ -189,8 +189,8 @@ class MailController extends AbstractController
         $resolver = new Resolver\AggregateResolver();
         $renderer->setResolver($resolver);
         $map = new Resolver\TemplateMapResolver(array(
-            'email/group-email' => dirname(__DIR__) . '../view/email/group-email.phtml',
-            'email/text-email'  => dirname(__DIR__) . '../view/email/text-email.phtml',
+            'email/group-email' => dirname(__DIR__) . '/../view/email/group-email.phtml',
+            'email/text-email'  => dirname(__DIR__) . '/../view/email/text-email.phtml',
         ));
         $resolver->attach($map);
 
