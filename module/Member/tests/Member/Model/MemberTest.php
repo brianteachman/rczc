1e44c836ef517847d7b96f373eb1756932dffef7
Brian Teachman <me@briant.me>
2013-05-07 11:45:12 -0700

    Refactored Member test.

--- module/Member/tests/Member/Model/MemberTest.php
+++ module/Member/tests/Member/Model/MemberTest.php
@@ -8,24 +8,38 @@ use PHPUnit_Framework_TestCase;
 class MemberTest extends PHPUnit_Framework_TestCase
 {
     protected $test_params = array('id' => 2,
-                                   'name'     => 'Brian Teachman',
-                                   'birthdate'  => '01/01/1900',
+                                   'first_name'     => 'Brian ',
+                                   'last_name'  => 'Teachman',
                                    'email' => 'you@your.tld',
-                                   'question' => 'When to pee?',
-                                   'deck' => 'Rider Waite',
-                                   'card_spread' => '5 card');
+                                   'home_phone' => '',
+                                   'work_phone' => '',
+                                   'street' => 'Some St.',
+                                   'city' => 'Bellingham',
+                                   'state' => 'WA',
+                                   'zipcode' => '98229',
+                                   'country' => 'USA',
+                                   'is_local' => 1,
+                                   'member_notes' => '',
+                                   'membership_type' => '',
+                                   'membership_renewal' => '',
+                                   'sangha_jobs' => '',
+                                   'volunteer_interest' => '',
+                                   'email_optin' => '',
+                                   'list_in_directory' => '',
+                                   'membership_notes' => '');
 
     public function testMemberInitialState()
     {
         $info = new Member();
 
         $this->assertNull($info->id, '"id" should initially be null');
-        $this->assertNull($info->name, '"name" should initially be null');
-        $this->assertNull($info->birthdate, '"birthdate" should initially be null');
+        $this->assertNull($info->first_name, '"first_name" should initially be null');
+        $this->assertNull($info->last_name, '"last_name" should initially be null');
         $this->assertNull($info->email, '"email" should initially be null');
-        $this->assertNull($info->question, '"question" should initially be null');
-        $this->assertNull($info->deck, '"deck" should initially be null');
-        $this->assertNull($info->card_spread, '"card_spread" should initially be null');
+        $this->assertNull($info->street, '"street" should initially be null');
+        $this->assertNull($info->city, '"city" should initially be null');
+        $this->assertNull($info->state, '"state" should initially be null');
+        $this->assertNull($info->zipcode, '"zipcode" should initially be null');
     }
 
     public function testExchangeArraySetsPropertiesCorrectly()
@@ -36,12 +50,13 @@ class MemberTest extends PHPUnit_Framework_TestCase
         $info->exchangeArray($data);
 
         $this->assertSame($data['id'], $info->id, '"artist" was not set correctly');
-        $this->assertSame($data['name'], $info->name, '"id" was not set correctly');
-        $this->assertSame($data['birthdate'], $info->birthdate, '"title" was not set correctly');
+        $this->assertSame($data['first_name'], $info->first_name, '"id" was not set correctly');
+        $this->assertSame($data['last_name'], $info->last_name, '"title" was not set correctly');
         $this->assertSame($data['email'], $info->email, '"artist" was not set correctly');
-        $this->assertSame($data['question'], $info->question, '"id" was not set correctly');
-        $this->assertSame($data['deck'], $info->deck, '"title" was not set correctly');
-        $this->assertSame($data['card_spread'], $info->card_spread, '"title" was not set correctly');
+        $this->assertSame($data['street'], $info->street, '"id" was not set correctly');
+        $this->assertSame($data['city'], $info->city, '"title" was not set correctly');
+        $this->assertSame($data['state'], $info->state, '"title" was not set correctly');
+        $this->assertSame($data['zipcode'], $info->zipcode, '"title" was not set correctly');
     }
 
     public function testExchangeArraySetsPropertiesToNullIfKeysAreNotPresent()
@@ -52,11 +67,12 @@ class MemberTest extends PHPUnit_Framework_TestCase
         $info->exchangeArray(array());
 
         $this->assertNull($info->id, '"id" should have defaulted to null');
-        $this->assertNull($info->name, '"name" should have defaulted to null');
-        $this->assertNull($info->birthdate, '"birthdate" should have defaulted to null');
+        $this->assertNull($info->first_name, '"first_name" should have defaulted to null');
+        $this->assertNull($info->last_name, '"last_name" should have defaulted to null');
         $this->assertNull($info->email, '"email" should have defaulted to null');
-        $this->assertNull($info->question, '"question" should have defaulted to null');
-        $this->assertNull($info->deck, '"deck" should have defaulted to null');
-        $this->assertNull($info->card_spread, '"card_spread" should have defaulted to null');
+        $this->assertNull($info->street, '"street" should have defaulted to null');
+        $this->assertNull($info->city, '"city" should have defaulted to null');
+        $this->assertNull($info->state, '"state" should have defaulted to null');
+        $this->assertNull($info->zipcode, '"zipcode" should have defaulted to null');
     }
 }
